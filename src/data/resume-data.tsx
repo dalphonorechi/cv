9d92795dd2a5346590d84304e991d44107ec0867
dalphonorechi <dalphonorechi@gmail.com>
2024-01-03 20:52:20 +0300

    Initial commit

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,28 +20,28 @@ import { LinkedInIcon } from "@/components/icons/LinkedInIcon";
 import { XIcon } from "@/components/icons/XIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Dalphon Orechifffff",
+  initials: "DO",
+  location: "Nairobi, Kenya, EAT",
+  locationLink: "https://www.google.com/maps/place/Nairobi",
   about:
     "Full Stack Engineer focused on building products with extra attention to details",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  personalWebsiteUrl: "https://dalphonorechi.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "info@dalphonorechi.com",
+    tel: "+254716109218",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/dalphonorechi",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/dalphon-orechi-1109821aa/",
         icon: LinkedInIcon,
       },
       {
@@ -53,10 +53,10 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Bachelor of Electrical and Telecommunication Engineering - Multimedia University of Kenya",
+      degree: "",
+      start: "2014",
+      end: "2019",
     },
   ],
   work: [
